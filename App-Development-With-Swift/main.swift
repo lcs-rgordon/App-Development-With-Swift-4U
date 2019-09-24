fe704e808cf2efa5bcc64c5cc2801667a29fc755
Russell Gordon <github@russellgordon.ca>
2019-09-24 10:14:30 -0400

    Initial Commit

---
+++ App-Development-With-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  App-Development-With-Swift
+//
+//  Created by Gordon, Russell on 2019-09-24.
+//  Copyright © 2019 Gordon, Russell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
